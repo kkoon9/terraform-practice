deb3005fb54516551620de63b874fb9be8cb5409
kkoon9 <rndrnjs2003@gmail.com>
2021-08-01 20:35:20 +0900

    feat: 웹 서버 배포해보기
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- main.tf
+++ main.tf
@@ -14,9 +14,25 @@ provider "aws" {
   region  = "ap-northeast-2"
 }
 
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instacne"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
 resource "aws_instance" "example" {
 	ami               = "ami-0ba5cd124d7a79612"
 	instance_type     = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.instance.id]
+	user_data = <<-EOF
+								#!/bin/bash
+								echo "Hello, World" > index.html
+								nohup busybox httpd -f -p 8080 &
+								EOF
 
 	tags = {
 		Name = "terraform-example"
