f00c5ad07437e04d93759c612acfbd517bd42f98
kkoon9 <rndrnjs2003@gmail.com>
2021-08-06 23:19:31 +0900

    feat: terraform - load balancer
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- main.tf
+++ main.tf
@@ -38,6 +38,27 @@ resource "aws_security_group" "instance" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
+
+resource "aws_security_group" "alb" {
+	name = "terraform-example-alb"
+
+	# 인바운드 HTTP 트래픽 허용
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	# 아웃바운드 트래픽 허용
+	ingress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
 resource "aws_instance" "example" {
 	ami               = "ami-0ba5cd124d7a79612"
 	instance_type     = "t2.micro"
@@ -72,8 +93,11 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-	launch_configuration = aws_launch_configuration.example.name
+	launch_configuration     = aws_launch_configuration.example.name
 	vpc_zone_identifier      = data.aws_subnet_ids.default.ids
+
+	target_group_arns = [aws_lb_target_group.asg.arn]
+	health_check_type = "ELB"
 
 	min_size = 2
 	max_size = 10
@@ -89,9 +113,60 @@ resource "aws_lb" "example" {
 		name 							 = "terraform-alb-example"
 		load_balancer_type = "application"
 		subnets 					 = data.aws_subnet_ids.default.ids
+		security_groups		 = [aws_security_group.alb.id]
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server" 
+resource "aws_lb_listener" "http" {
+	load_balancer_arn = aws_lb.example.arn
+	port 						 	= 80
+	protocol					= "HTTP"
+
+	# 기본값으로 단순한 404 페이지 오류를 반환합니다
+	default_action {
+		type = "fixed-response"
+
+		fixed_response {
+			content_type = "text/plain"
+			message_body = "404: page not found"
+			status_code  = 404
+		}
+	}
+}
+
+resource "aws_lb_target_group" "asg" {
+	name     = "tarraform-asg-example"
+	port     = var.server_port
+	protocol = "HTTP"
+	vpc_id   = data.aws_vpc.default.id
+
+	health_check {
+		path 								= "/"
+		protocol 						= "HTTP"
+		matcher							= "200"
+		interval						= 15
+		timeout							= 3
+		healthy_threshold		= 2
+		unhealthy_threshold = 2
+	}
+}
+
+resource "aws_lb_listener_rule" "asg" {
+	listener_arn = aws_lb_listener.http.arn
+	priority		 = 100
+
+	condition {
+		path_pattern {
+			values = ["*"]
+		}
+	}
+
+	action {
+		type  					 = "forward"
+		target_group_arn = aws_lb_target_group.asg.arn
+	}
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer" 
 }
