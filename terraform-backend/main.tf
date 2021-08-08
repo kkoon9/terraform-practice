ecb0ef2221cc5f1ac0160590a7b6f980c82aea3e
kkoon9 <rndrnjs2003@gmail.com>
2021-08-08 20:24:34 +0900

    delete: 실수한 코드 삭제
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -1,16 +1,3 @@
-terraform {
-	backend "s3" {
-		# 이전에 생성한 버킷 이름으로 변경
-		bucket 	 = "terraform-up-and-running-state-kkoon9" 
-		key 	   = "global/s3/terraform.tfstate"
-	  region   = "ap-northeast-2"
-
-		# 이전에 생성한 다이나모DB 테이블 이름으로 변경
-		dynamodb_table = "terraform-up-and-running-locks"
-		encrypt 			 = true
-	}
-}
-
 provider "aws" {
   region  = "ap-northeast-2"
 }
