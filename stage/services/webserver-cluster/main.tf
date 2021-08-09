78f5e1e1d6286b88f639c439dde9a79c33b58c07
kkoon9 <rndrnjs2003@gmail.com>
2021-08-09 20:45:55 +0900

    fix: 디렉터리 재구성
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -169,18 +169,3 @@ resource "aws_lb_listener_rule" "asg" {
 		target_group_arn = aws_lb_target_group.asg.arn
 	}
 }
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer" 
-}
-
-output "s3_bucket_arn" {
-	value 			= aws_s3_bucket.terraform_state.arn
-	description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-	value 			= aws_dynamodb_table.terraform_locks.name
-	description = "The name of the DynamoDB table"	
-}
